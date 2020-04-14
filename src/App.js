d969f89de8dbd9f829cc71220d3d905710e48670
Angel Juarez <angeljuarez729@gmail.com>
2020-04-14 01:13:05 -0400

    todo list with native react state being used

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {
+          text: 'create react app',
+        },
+        {
+          text: 'create actions, action types, and action creators',
+        },
+        {
+          text: 'create reducers',
+        },
+        {
+          text: 'create store'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {
+              this.state.todos.map(todo => <li>{ todo.text }</li>)
+            }
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
